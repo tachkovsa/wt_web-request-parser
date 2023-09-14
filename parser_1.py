fa1bbaa0fe77d8d58cfbbd289866f73239e6c831
Тачков Сергей Александрович <tachkovsa@ya.ru>
2023-09-14 09:48:30 +0300

    ref(parser): add progress bar

--- parser_1.py
+++ parser_1.py
@@ -29,10 +29,6 @@ for file_path in log_files:
     file_date = re.match(r"web\_request\-(\d\d\d\d\-\d\d\-\d\d)\.log", file_name).group(1)
 
     with open(file_path) as fp:
-        # while True:
-            # file_lines = fp.readlines(65536)
-            # if not file_lines:
-            #     break
         file_lines = fp.readlines()
 
         line_number = 0
@@ -40,14 +36,12 @@ for file_path in log_files:
 
         butch_values = "";
         for line in file_lines:
-            # sys.stdout.write('\r')
-            # # the exact output you're looking for:
-            # sys.stdout.write("[%-20s] %d%%" % ('='*int(20*percent), 100*percent))
-            # sys.stdout.flush()
-
             line_number += 1
-            percent = int((line_number / total_lines) * 100)
-            print("{percent}%   {line_number} / {total_lines}".format(line_number = line_number, total_lines = total_lines, percent = percent))
+            percent = (line_number / total_lines)
+
+            sys.stdout.write('\r')
+            sys.stdout.write("[%-20s] %d%%" % ('='*int(20*percent), percent*100))
+            sys.stdout.flush()
             
             separated_line = line.split(" ");
             
@@ -56,7 +50,7 @@ for file_path in log_files:
     
             date, time, ip, login, url, _, method, path, _, _, _, _, _, navigator = separated_line
             
-            login = login.replace("\"", "") #.replace("\\", "\\\\")
+            login = login.replace("\"", "")
             navigator = navigator.replace("\"", "")[:-1]
 
             butch_values += '''(
@@ -86,27 +80,3 @@ butch_values = ""
 
 cursor.close()
 db.close()
-
-# with open("./logs/web_request-2022-07-11.log") as fp:
-#     file_name = os.path.basename(fp.name)
-
-#     file_date = re.match(regex, file_name).group(1)
-
-
-#     print(file_date)
-
-
-    # for matchNum, match in enumerate(matches, start=1):
-    #     print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-        
-    #     for groupNum in range(0, len(match.groups())):
-    #         groupNum = groupNum + 1
-            
-    #         print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
-
-    # print(fileName)
-
-    # Lines = fp.readlines()
-    # for line in Lines:
-    #     count += 1
-    #     print("Line{}: {}".format(count, line.strip()))
